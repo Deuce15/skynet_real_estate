aee710cf4018417f4f388015e430955989a7a02d
Deuce15 <isaiahnyamu@gmail.com>
2021-03-23 23:09:24 +0300

    First Commit

---
+++ listings/choices.py
@@ -0,0 +1,39 @@
+bedroom_choices = {
+  '1':1,
+  '2':2,
+  '3':3,
+  '4':4,
+  '5':5,
+  '6':6,
+  '7':7,
+  '8':8,
+  '9':9,
+  '10':10
+  }
+
+price_choices = {
+  '10000':'KSH10,000',
+  '20000':'KSH20,000',
+  '30000':'KSH30,000',
+  '40000':'KSH40,000',
+  '50000':'KSH50,000',
+  '60000':'KSH60,000',
+  '70000':'KSH70,000',
+  '80000':'KSH80,000',
+  '90000':'KSH90,000',
+  '1000000':'KSH1M+',
+}
+
+state_choices = {
+        
+       'GA': 'Gate A',
+       'GB': 'Gate B' ,
+       'GC': 'Gate C',
+       'G':'Gachororo',
+       'K':'Kiaria',
+       'BF':'Benvar Farm',
+       'AC':'Alpha Courts',
+       'W':'Witeithie',
+       'JSE': 'Juja South Estate ',
+       'KA':'Kalimoni',
+       }
